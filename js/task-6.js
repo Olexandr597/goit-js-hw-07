09392c93264f075bb8e4e912da1f4885a1de4ff8
Olexandr597 <Olexandr597@gmail.com>
2024-01-17 00:00:20 +0200

    task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,38 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const boxes = document.querySelector('#boxes');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]')
+
+let amount = 0;
+
+input.addEventListener('input', (event) => {
+  amount = event.currentTarget.value;
+});
+
+btnCreate.addEventListener('click', createBoxes);
+
+function createBoxes() {
+  for (let i = 1; i <= amount; i++) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${20 + 10 * i}px`; 
+    box.style.height = `${20 + 10 * i}px`;
+
+    boxes.append(box);
+  }
+};
+
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  input.value = '';
+  for (let i = 0; i < boxes.children.length; i++) {
+    boxes.lastChild.remove();
+    i -= 1; 
+  };
+  amount = 0;
+};
