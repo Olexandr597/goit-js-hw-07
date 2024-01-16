da43e7625b0bbb33ea3c6c0c250eb8663a230033
Olexandr597 <Olexandr597@gmail.com>
2024-01-16 23:28:09 +0200

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('form');
 
+let user = {
+  email: "",
+  password: "",
+};
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const forma = event.target;
+    const email = forma.elements.email.value;
+    const password = forma.elements.value;
+
+    if (email === "" || password === "" ){
+        return alert('All form fields must be filled in');
+    }
+    else{
+        user.email = email;
+        user.password = password;
+        console.log(user);
+
+    }
+    form.reset();
+})
